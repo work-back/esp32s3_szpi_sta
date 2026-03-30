748516db3bc6fa47a9af2bd347adee237026c4c4
langyanjun <langyanjun0510@a-su.com.cn>
2026-03-30 14:07:03 +0800

    fix build warnning

--- src/wscli.c
+++ src/wscli.c
@@ -95,11 +95,13 @@ static int connect_cb(int sock, struct http_request *req, void *user_data)
 	return 0;
 }
 
+#if 0
 static ssize_t sendall_with_ws_api(int sock, const void *buf, size_t len)
 {
 	return websocket_send_msg(sock, buf, len, WEBSOCKET_OPCODE_DATA_TEXT,
 				  true, true, SYS_FOREVER_MS);
 }
+#endif
 
 ssize_t wscli_send(int sock, const void *buf, size_t len)
 {
@@ -144,7 +146,6 @@ int wscli_init(void)
 
 	int32_t timeout = 3 * MSEC_PER_SEC;
 	struct sockaddr_in addr4;
-	int ret;
 
 	if (!IS_ENABLED(CONFIG_NET_IPV4)) {
 		LOG_ERR("Ipv4 is not enabled.");
@@ -181,7 +182,7 @@ int wscli_init(void)
 
 	g_websock4 = websocket_connect(g_sock4, &req, timeout, "IPv4");
 	if (g_websock4 < 0) {
-		LOG_ERR("Cannot connect to %s:%d, ret:%d", SERVER_ADDR4, SERVER_PORT, g_websock4);
+		LOG_ERR("Cannot connect to %s:%d, websocket:%d", SERVER_ADDR4, SERVER_PORT, g_websock4);
 		close(g_sock4);
 		g_sock4 = -1;
 		return -1;
