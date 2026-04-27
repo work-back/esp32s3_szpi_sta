8cfcf16c07b7fcd789120a29651dc5958c51daca
lyj051031448 <lyj051031448@163.com>
2026-04-27 23:00:02 +0800

    add auto.sh for west cmd

---
+++ auto.sh
@@ -0,0 +1,25 @@
+#/bin/bash
+
+ESP_DEV=/dev/ttyACM0
+BOARD=esp32s3_wave/esp32s3/procpu
+PRJ=sta
+
+build_all() {
+    if [ ! -z $1 ] ; then
+        PRJ=$1
+    fi
+    west build -p always -b ${BOARD} --sysbuild ${PRJ}
+}
+
+flash_esp() {
+    west flash --esp-device ${ESP_DEV}
+}
+
+monitor_esp() {
+    west espressif monitor -p ${ESP_DEV}
+}
+
+erase_and_flash_esp() {
+    west flash --erase --esp-device ${ESP_DEV}
+}
+
